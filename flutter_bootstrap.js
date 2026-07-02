6e0d0f644a8e793e4192b3fb57dda1b608ea7a12
bryannamd <bryannamd@users.noreply.github.com>
2026-07-02 12:35:51 +0000

    deploy: 8e6595a4a60f599630245b5fb69e1486572446ea (auto, dev/test) bryannamd/hanja-study-app@8e6595a4a60f599630245b5fb69e1486572446ea

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "276244460" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2306269521" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
